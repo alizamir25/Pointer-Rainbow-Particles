9dde1a4b5688dad42acc84a3c047359879396927
alizamir25 <ali0786zamir@gmail.com>
2024-10-12 17:04:08 +0100

    Patch

--- app.js
+++ app.js
@@ -63,79 +63,79 @@ class PointerParticle{
       };
       this.hue=0;
     }
-    connectedCallback() {
-      const canvas = document.createElement("canvas");
-      const sheet = new CSSStyleSheet();
-      this.shadowroot = this.attachShadow({ mode: "open" });
+    connectedCallback(){
+      const canvas=document.createElement("canvas");
+      const sheet=new CSSStyleSheet();
+      this.shadowroot=this.attachShadow({ mode:"open"});
       sheet.replaceSync(PointerParticles.css);
-      this.shadowroot.adoptedStyleSheets = [sheet];
+      this.shadowroot.adoptedStyleSheets=[sheet];
       this.shadowroot.append(canvas);
-      this.canvas = this.shadowroot.querySelector("canvas");
-      this.ctx = this.canvas.getContext("2d");
+      this.canvas=this.shadowroot.querySelector("canvas");
+      this.ctx=this.canvas.getContext("2d");
       this.setCanvasDimensions();
       this.setupEvents();
-      this.timePrevious = performance.now();
+      this.timePrevious=performance.now();
       this.animateParticles();
     }
-    createParticles(event, { count, speed, spread }) {
+    createParticles(event,{ount, speed, spread}){
       this.setPointerValues(event);
-      for (let i = 0; i < count; i++) {
+      for (let i=0;i<count;i++){
         this.particles.push(new PointerParticle(spread, speed, this));
       }
     }
-    setPointerValues(event) {
-      this.pointer.x = event.x - this.offsetLeft;
-      this.pointer.y = event.y - this.offsetTop;
-      this.pointer.mx = event.movementX;
-      this.pointer.my = event.movementY;
+    setPointerValues(event){
+      this.pointer.x=event.x-this.offsetLeft;
+      this.pointer.y=event.y-this.offsetTop;
+      this.pointer.mx=event.movementX;
+      this.pointer.my=event.movementY;
     }
-    setupEvents() {
-      const parent = this.parentNode;
-      parent.addEventListener("click", (event) => {
-        this.createParticles(event, {
-          count: 300,
-          speed: Math.random() + 1,
-          spread: Math.random() + 50
+    setupEvents(){
+      const parent=this.parentNode;
+      parent.addEventListener("click",(event)=>{
+        this.createParticles(event,{
+          count: 500,
+          speed: Math.random()+1,
+          spread: Math.random()+50
         });
       });
-      parent.addEventListener("pointermove", (event) => {
-        this.createParticles(event, {
-          count: 20,
+      parent.addEventListener("pointermove",(event)=>{
+        this.createParticles(event,{
+          count: 50,
           speed: this.getPointerVelocity(event),
           spread: 1
         });
       });
-      window.addEventListener("resize", () => this.setCanvasDimensions());
+      window.addEventListener("resize",()=>this.setCanvasDimensions());
     }
-    getPointerVelocity(event) {
-      const a = event.movementX;
-      const b = event.movementY;
-      const c = Math.floor(Math.sqrt(a * a + b * b));
+    getPointerVelocity(event){
+      const a=event.movementX;
+      const b=event.movementY;
+      const c=Math.floor(Math.sqrt(a*a+b*b));
       return c;
     }
-    handleParticles() {
-      for (let i = 0; i < this.particles.length; i++) {
+    handleParticles(){
+      for (let i=0;i<this.particles.length;i++){
         this.particles[i].update();
-        if (this.particles[i].size <= 0.1) {
+        if (this.particles[i].size<=0.2){
           this.particles.splice(i, 1);
           i--;
         }
       }
     }
-    setCanvasDimensions() {
-      const rect = this.parentNode.getBoundingClientRect();
-      this.canvas.width = rect.width;
-      this.canvas.height = rect.height;
+    setCanvasDimensions(){
+      const rect=this.parentNode.getBoundingClientRect();
+      this.canvas.width=rect.width;
+      this.canvas.height=rect.height;
     }
-    animateParticles() {
-      requestAnimationFrame(() => this.animateParticles());
-      const timeNow = performance.now();
-      const timePassed = timeNow - this.timePrevious;
-      if (timePassed < this.msPerFrame) return;
-      const excessTime = timePassed % this.msPerFrame;
-      this.timePrevious = timeNow - excessTime;
+    animateParticles(){
+      requestAnimationFrame(()=>this.animateParticles());
+      const timeNow=performance.now();
+      const timePassed=timeNow-this.timePrevious;
+      if (timePassed<this.msPerFrame) return;
+      const excessTime=timePassed%this.msPerFrame;
+      this.timePrevious=timeNow-excessTime;
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.hue = this.hue > 360 ? 0 : (this.hue += 3);
+      this.hue=this.hue>360?0:(this.hue+=3);
       this.handleParticles();
     }
   }
